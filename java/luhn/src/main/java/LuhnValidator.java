a7df3343d90d97d3c6367d50069910241a631430
Garrett Arant <garrettarant@MacBook-Pro-6.local>
2018-04-12 15:27:27 -0400

    Luhn Done

--- java/luhn/src/main/java/LuhnValidator.java
+++ java/luhn/src/main/java/LuhnValidator.java
@@ -1,7 +1,33 @@
+import java.util.regex.Pattern;
+
 class LuhnValidator {
 
     boolean isValid(String candidate) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        int sum = 0;
+        String candidateWithOutSpace = candidate.replaceAll(" ", "");
+        if (candidateWithOutSpace.length() <= 1 || !Pattern.matches("[\\d]+", candidateWithOutSpace)) {
+            return false;
+        }
+            String[] stringArray = candidateWithOutSpace.split("");
+            Integer[] integerArray = new Integer[stringArray.length];
 
+            for (int i = 0; i < integerArray.length; i++) {
+                integerArray[i] = Integer.parseInt(stringArray[i]);
+            }
+
+
+            for (int i = integerArray.length - 2; i >= 0; i-= 2){
+                integerArray[i] = integerArray[i]*2;
+                if (integerArray[i] > 9) {
+                    integerArray[i] = integerArray[i] - 9;
+                }
+            }
+
+
+            for (int i = 0; i < integerArray.length; i++) {
+                sum += integerArray[i];
+            }
+        return (sum % 10 == 0);
+
+    }
 }
